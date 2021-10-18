17864e06949fa8163001879210cdb47a762c54e5
Jesse Ditson <jditson@quip.com>
2021-10-18 10:52:32 -0700

    simplicity updates

--- capture.sh
+++ capture.sh
@@ -3,14 +3,17 @@
 mkdir out
 
 # can provide as an env var
-[[ -z "$STREAM_URL" ]] STREAM_URL=$1
+[[ -z "$STREAM_URL" ]] && STREAM_URL=$1
+
 # A simple local rtmp server will produce this.
 # Por ejemplo https://github.com/sallar/mac-local-rtmp-server
-[[ -z "$STREAM_URL" ]] && STREAM_URL="rtmp://127.0.0.1/live/S1gsTHqCw"
+[[ "${STREAM_URL:0:4}" != "rtmp" ]] && STREAM_URL="rtmp://127.0.0.1/live/$STREAM_URL"
+[[ -z "$STREAM_URL" ]] && STREAM_URL="rtmp://127.0.0.1/live/stream"
 
 caffeinate -s\
- ffmpeg -i "$STREAM_URL" \
- -r 1/10\
+ ./forever.sh\
+ ffmpeg -i "$STREAM_URL"\
+ -r 1/20\
  "out/img-%05d.jpg"
 
 mv out $(date +'%m-%d-%Y-%H%M')
--- restream.sh
+++ restream.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # can provide as an env var
-[[ -z "$STREAM_URL" ]] STREAM_URL=$1
+[[ -z "$STREAM_URL" ]] && STREAM_URL=$1
+
 # A simple local rtmp server will produce this.
 # Por ejemplo https://github.com/sallar/mac-local-rtmp-server
-[[ -z "$STREAM_URL" ]] && STREAM_URL="rtmp://127.0.0.1/live/S1gsTHqCw"
+[[ "${STREAM_URL:0:4}" != "rtmp" ]] && STREAM_URL="rtmp://127.0.0.1/live/$STREAM_URL"
+[[ -z "$STREAM_URL" ]] && STREAM_URL="rtmp://127.0.0.1/live/stream"
 
 # can provide as an env var
-[[ -z "$CAST_URL" ]] CAST_URL=$1
+[[ -z "$CAST_URL" ]] && CAST_URL=$1
 # Youtube produces this for you
-[[ -z "$CAST_URL" ]] && CAST_URL="rtmp://a.rtmp.youtube.com/live2/something"
+[[ -z "$CAST_URL" ]] && CAST_URL="rtmp://a.rtmp.youtube.com/live2/qv87-3fu0-x8m8-syvz-6rt5"
 
 caffeinate -s\
+ ./forever.sh\
  ffmpeg -i "$STREAM_URL"\
  -f lavfi -i anullsrc\
  -f flv "$CAST_URL"
